da3090448ea184e77cef5186887de2a371c99c45
Richard Eames <reames@asymmetricventures.com>
2014-05-21 15:15:14 -0600

    Added better error information to the exceptions raised

--- aggcat/exceptions.py
+++ aggcat/exceptions.py
@@ -1,6 +1,16 @@
+from .parser import Objectify
 
 class HTTPError(Exception):
     """Http Error Exception"""
     def __init__(self, *args, **kwargs):
         self.response = kwargs.pop('response', None)
+        self.err_obj = None
+        self.status_code = None
+        if self.response is not None:
+            self.status_code = self.response.status_code
+            try:
+                self.err_obj = Objectify(self.response.content).get_object()
+            except:
+                pass
+        
         super(HTTPError, self).__init__(*args, **kwargs)
